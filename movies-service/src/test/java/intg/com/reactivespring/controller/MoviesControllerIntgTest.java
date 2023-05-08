bbe9bf4eefe51444e40f1e68f94880f98771b9ad
Camilo Barros <Camilo.Barros@Millicom.com>
2023-05-08 12:58:41 -0400

    Add WireMock dependency and integration tests
    This commit adds the WireMock dependency to the project and includes a set of integration tests that utilize WireMock to mock external dependencies. The tests are located in the "test/integration" directory and use JSON files to configure WireMock's responses to specific requests.
    
    The "mappings" directory contains several JSON files that define the expected requests and responses for the mocked endpoints. These files are used by WireMock to simulate the external dependencies during the integration tests.
    
    Overall, this commit improves the robustness and reliability of the project by ensuring that external dependencies are properly mocked and tested.

--- movies-service/src/test/java/intg/com/reactivespring/controller/MoviesControllerIntgTest.java
+++ movies-service/src/test/java/intg/com/reactivespring/controller/MoviesControllerIntgTest.java
@@ -1,4 +1,142 @@
 package com.reactivespring.controller;
 
+import com.reactivespring.domain.Movie;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.contract.wiremock.AutoConfigureWireMock;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+import static com.github.tomakehurst.wiremock.client.WireMock.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("test")
+@AutoConfigureWebTestClient
+@AutoConfigureWireMock(port = 8084) //spin up wiremock server on port 8084
+@TestPropertySource(
+        properties = {
+                "restClient.moviesInfoUrl=http://localhost:8084/v1/movie-info",
+                "restClient.reviewsUrl=http://localhost:8084/v1/reviews"
+        }
+)
 public class MoviesControllerIntgTest {
+
+    @Autowired
+    WebTestClient webTestClient;
+
+    @Test
+    void retrieveMovieById() {
+        //given
+        var movieId = "abc";
+        stubFor(get(urlEqualTo("/v1/movie-info"+"/"+movieId))
+                .willReturn(
+                        aResponse()
+                        .withHeader("Content-Type", "application/json")
+                        .withBodyFile("movieInfo.json")));
+
+        stubFor(get(urlPathEqualTo("/v1/reviews"))
+                .willReturn(
+                        aResponse()
+                                .withHeader("Content-Type", "application/json")
+                                .withBodyFile("reviews.json")));
+
+
+
+        //when
+        webTestClient
+                .get()
+                .uri("/v1/movies/{id}", movieId)
+                .exchange()
+                .expectStatus()
+                .isOk()
+                .expectBody(Movie.class)
+                .consumeWith(movieEntityExchangeResult -> {
+                    var movie = movieEntityExchangeResult.getResponseBody();
+                    assert movie != null;
+                    assert movie.getReviewList().size() == 2;
+                    assertEquals("Batman Begins", movie.getMovieInfo().getName());
+                });
+    }
+
+    @Test
+    void retrieveMovieById_404() {
+        //given
+        var movieId = "abc";
+        stubFor(get(urlEqualTo("/v1/movie-info"+"/"+movieId))
+                .willReturn(aResponse().withStatus(404)));
+
+        stubFor(get(urlPathEqualTo("/v1/reviews"))
+                .willReturn(
+                        aResponse()
+                                .withHeader("Content-Type", "application/json")
+                                .withBodyFile("reviews.json")));
+
+
+
+        //when
+        webTestClient
+                .get()
+                .uri("/v1/movies/{id}", movieId)
+                .exchange()
+                .expectStatus()
+                .is4xxClientError()
+                .expectBody(String.class)
+                .isEqualTo("There is no info Available for the movieId : abc");
+    }
+
+    @Test
+    void retrieveMovieById_reviews_404() {
+        //given
+        var movieId = "abc";
+        stubFor(get(urlEqualTo("/v1/movie-info"+"/"+movieId))
+                .willReturn(
+                        aResponse()
+                                .withHeader("Content-Type", "application/json")
+                                .withBodyFile("movieInfo.json")));
+
+        stubFor(get(urlPathEqualTo("/v1/reviews"))
+                .willReturn(aResponse().withStatus(404)));
+
+
+
+        //when
+        webTestClient
+                .get()
+                .uri("/v1/movies/{id}", movieId)
+                .exchange()
+                .expectStatus()
+                .isOk()
+                .expectBody(Movie.class)
+                .consumeWith(movieEntityExchangeResult -> {
+                    var movie = movieEntityExchangeResult.getResponseBody();
+                    assert movie != null;
+                    assert movie.getReviewList().size() == 0;
+                    assertEquals("Batman Begins", movie.getMovieInfo().getName());
+                });
+    }
+
+    @Test
+    void retrieveMovieById_5XX() {
+        //given
+        var movieId = "abc";
+        stubFor(get(urlEqualTo("/v1/movie-info"+"/"+movieId))
+                .willReturn(aResponse()
+                        .withStatus(500)
+                        .withBody("Movie Info Service is Down")));
+
+        //when
+        webTestClient
+                .get()
+                .uri("/v1/movies/{id}", movieId)
+                .exchange()
+                .expectStatus()
+                .is5xxServerError()
+                .expectBody(String.class)
+                .isEqualTo("Server Exception while calling movies-info-service : Movie Info Service is Down");
+    }
 }
